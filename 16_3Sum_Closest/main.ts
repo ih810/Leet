efcf8781480079bea36974992ce8525eee89e7eb
iv <ivanhong810@gmail.com>
2024-07-21 12:51:35 +0100

    Update

---
+++ 16_3Sum_Closest/main.ts
@@ -0,0 +1,31 @@
+/*
+Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+Return the sum of the three integers.
+You may assume that each input would have exactly one solution.
+*/
+
+function threeSumClosest(nums: number[], target: number): number {
+    nums.sort((a, b) => a - b);
+    let closestSum = nums[0] + nums[1] + nums[2];
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const currentSum = nums[i] + nums[left] + nums[right];
+
+            if (Math.abs(target - currentSum) < Math.abs(target - closestSum)) {
+                closestSum = currentSum;
+            }
+
+            if (currentSum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return closestSum;
+}
