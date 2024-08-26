04884cc9b9a7641a671982aefb613fa6e96452af
ih810 <ivanhong810@gmail.com>
2024-08-26 17:20:12 +0100

    Update

---
+++ 45_Jump_Game_II/main.ts
@@ -0,0 +1,26 @@
+/*
+You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+
+Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+- 0 <= j <= nums[i] and
+- i + j < n
+
+Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+*/
+
+function jump(nums: number[]): number {
+    let jumps = 0;
+    let currentEnd = 0;
+    let farthest = 0;
+    
+    for (let i = 0; i < nums.length - 1; i++) {
+        farthest = Math.max(farthest, i + nums[i]);
+        
+        if (i === currentEnd) {
+            jumps++;
+            currentEnd = farthest;
+        }
+    }
+    
+    return jumps;
+}
