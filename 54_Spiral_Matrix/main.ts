bfc94381930c4d34d965382970b453017eabb13e
ih810 <ivanhong810@gmail.com>
2024-10-03 09:27:53 +0100

    Update

---
+++ 54_Spiral_Matrix/main.ts
@@ -0,0 +1,44 @@
+/*
+Given an m x n matrix, return all elements of the matrix in spiral order.
+
+Example 1:
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+
+Example 2:
+Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+*/
+
+function spiralOrder(matrix: number[][]): number[] {
+    const result: number[] = [];
+    let top = 0, bottom = matrix.length - 1, left = 0, right = matrix[0].length - 1;
+    
+    while (top <= bottom && left <= right) {
+        for (let i = left; i <= right; i++) {
+            result.push(matrix[top][i]);
+        }
+        top++;
+        
+        for (let i = top; i <= bottom; i++) {
+            result.push(matrix[i][right]);
+        }
+        right--;
+        
+        if (top <= bottom) {
+            for (let i = right; i >= left; i--) {
+                result.push(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+        
+        if (left <= right) {
+            for (let i = bottom; i >= top; i--) {
+                result.push(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+    
+    return result;
+}
